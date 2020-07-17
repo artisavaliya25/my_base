e7fb632ddc044567520413d4a8d75a89e405b974
artisavaliya25 <68413331+artisavaliya25@users.noreply.github.com>
2020-07-17 15:44:22 +0530

    Update settings.py

--- learning_user/settings.py
+++ learning_user/settings.py
@@ -87,11 +87,11 @@ DATABASES = {
 # Password validation
 # https://docs.djangoproject.com/en/1.10/ref/settings/#auth-password-validators
 PASSWORD_HASHERS=[
-    'django.contib.auth.hashers.Argon2PasswordHasher',
-    'django.contib.auth.hashers.BCryptSHA256PasswordHasher',
-    'django.contib.auth.hashers.BCryptPasswordHasher',
-    'django.contib.auth.hashers.PBKDF2PasswordHasher',  
-    'django.contib.auth.hashers.PBKDF2SHA1PasswordHasher',
+    'django.contrib.auth.hashers.Argon2PasswordHasher',
+    'django.contrib.auth.hashers.BCryptSHA256PasswordHasher',
+    'django.contrib.auth.hashers.BCryptPasswordHasher',
+    'django.contrib.auth.hashers.PBKDF2PasswordHasher',  
+    'django.contrib.auth.hashers.PBKDF2SHA1PasswordHasher',
 ]
 AUTH_PASSWORD_VALIDATORS = [
     {
